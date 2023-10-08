1e777c0e00d2a0f6baf21812113bbb4e07ec89b1
Branchey <70140890+CZboop@users.noreply.github.com>
2023-10-08 12:08:19 +0100

    added docstrings to and cleaned up services and utils

--- services.py
+++ services.py
@@ -1,7 +1,7 @@
 import db.database as _db
 import db.models as _models
 import db.schemas as _schemas
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Dict
 from sqlalchemy.sql.expression import func, select
 import random
 from fastapi import HTTPException
@@ -9,18 +9,25 @@ from fastapi import HTTPException
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
+'''
+FUNCTIONS TO INTERACT WITH THE DATABASE
+'''
+
 def _setup_tables():
+    '''
+    Creates the "words" table based on declarative base model
+    '''
     return _db.Base.metadata.create_all(bind = _db.engine)
 
-def get_db():
-    db = _db.SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def create_word(word: _schemas.CreateWord) -> _schemas.Word:
-    word = _models.Word(**word) # unpacking dict passed in as kwargs
+def create_word(word: Dict[str, str]) -> _schemas.Word:
+    '''
+    Adds a word object to the database from a dict of properties
+    Args:
+        word (Dict[str, str]): Dict of kwargs to be passed as properties to word constructor
+    Returns:
+        word (_schemas.Word): Word based on pydantic base model
+    '''
+    word = _models.Word(**word)
     db = _db.SessionLocal()
 
     db.add(word)
@@ -31,6 +38,13 @@ def create_word(word: _schemas.CreateWord) -> _schemas.Word:
     return _schemas.Word.from_orm(word)
 
 def get_word_with_given_relation(relation: str) -> _schemas.Word:
+    '''
+    Takes a string of word relation and returns an object representing a row from the word table, which has words with that relation
+    Args:
+        relation (str): String to be checked as being not null when selecting a random word, should match a column in the word table
+    Returns:
+        random_word (_schemas.Word): Word object created from a row in the words table, randomly selected from words with the given relation
+    '''
     db = _db.SessionLocal()
     words_with_relation = db.query(_models.Word).filter(getattr(_models.Word, relation) != "")
     random_word = words_with_relation.order_by(func.random()).first()
@@ -41,6 +55,11 @@ def get_word_with_given_relation(relation: str) -> _schemas.Word:
     return random_word
 
 def get_random_word() -> _schemas.Word:
+    '''
+    Returns a word object created from a completely random word in the database
+    Returns:
+        random_word (_schemas.Word): A random word from the "words" table as a word object
+    '''
     db = _db.SessionLocal()
     try:
         random_word = random.choice(db.query(_models.Word).all())
@@ -51,6 +70,11 @@ def get_random_word() -> _schemas.Word:
     return random_word
 
 def get_all_words() -> List[_schemas.Word]:
+    '''
+    Returns all words from the database
+    Returns:
+        all_words (List[_schemas.Word]): A list of word objects representing every word in the "words" table
+    '''
     db = _db.SessionLocal()
     all_words = db.query(_models.Word).all()
     db.close()
--- utils.py
+++ utils.py
@@ -10,36 +10,90 @@ from nltk.stem.porter import *
 from fuzzywuzzy import fuzz
 import pandas as pd
 
+'''
+REUSABLE UTILS CLASS TO PASS IN TO OTHER OBJECTS
+PRIMARILY VALIDATION AND RETRIEVAL OF WORDNET DATA
+'''
+
 class Utils:
     def __init__(self):
+        '''
+        Constructor creating an object of type Utils. Takes no args, but sets some attributes to be used in methods
+        '''
         self.wn = wn
         self.words_in_wn = list(set(i for i in wn.words()))
         self.wn_length = len(self.words_in_wn)
         self.stemmer = PorterStemmer()
 
     def _get_fuzz_score(self, word1: str, word2: str) -> int:
+        '''
+        Takes two words and returns fuzzy matching score between them
+        Args:
+            word1 (str): First word to be compared
+            word2 (str): Second word to be compared
+        Returns:
+            fuzz_ratio (int): Score of fuzzy similarity from 0 to 100
+        '''
         return fuzz.ratio(word1, word2)
     
     def get_random_word(self) -> str:
+        '''
+        Returns a random word from WordNet
+        Returns:
+            random_word (str): A random word from wordnet
+        '''
         random_index = random.randrange(0, self.wn_length)
         random_word = self.words_in_wn[random_index]
         return random_word
     
     def get_synsets(self, word: str) -> Tuple[List[nltk.corpus.reader.wordnet.Synset], int]:
-        synset = wn.synsets(word)
-        num_synsets = len(synset)
-        return synset, num_synsets # note will have variable number of synsets for different words
+        '''
+        Takes a word and returns its synsets and the number of synsets it has
+        Args:
+            word (str): A word whose synsets we want
+        Returns:
+            synsets (List[nltk.corpus.reader.wordnet.Synset]): The synsets found for the word passed in
+            num_synsets (int): The number of synsets found
+        '''
+        synsets = wn.synsets(word)
+        num_synsets = len(synsets)
+        return synsets, num_synsets
 
     def get_words_from_synsets(self, synsets: List[nltk.corpus.reader.wordnet.Synset]) -> List[str]:
+        '''
+        Takes a list of synsets and returns all words from the synsets
+        Args:
+            synsets (List[nltk.corpus.reader.wordnet.Synset]): List of synsets whose words will be returned
+        Returns:
+            words (List[str]): List of words found from synsets
+        '''
         lemmas = list(chain.from_iterable([synset.lemmas() for synset in synsets]))
         words = list([str(item.name()) for item in lemmas])
         return words
     
     def get_lemmas_from_synset(self, synset: nltk.corpus.reader.wordnet.Synset) -> List[nltk.corpus.reader.wordnet.Lemma]:
+        '''
+        Takes a synset and returns its lemmas
+        Args:
+            synset (nltk.corpus.reader.wordnet.Synset): A synset for lemmas to be returned from
+        Returns:
+            lemmas (List[nltk.corpus.reader.wordnet.Lemma]): List of lemmas found from synset
+        '''
         lemmas = synset.lemmas()
         return lemmas
 
     def _validate_related_words(self, word1: str, related: List[str]) -> List[str]:
+        '''
+        Takes a word and list of words related to it, returns the related words list with invalid words removed
+        Args:
+            word1 (str): A word to be checked against its related words
+            related (List[str]): A list of related words to be validated against main word
+        Returns:
+            valid_words (List[str]): The original list of related words, with words removed if:
+                - They are too similar to word1
+                - They contain numbers
+                - They contain an underscore indicating they are actually multiple words
+        '''
         valid_words = []
         for word in related:
             # exclude words with same stem e.g. don't want look and looked as synonyms
